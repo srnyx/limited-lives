4cfe63eefffd0d503ebd75937983701a19e419ae
srnyx <25808801+srnyx@users.noreply.github.com>
2023-08-01 22:17:06 -0400

    Bump version (3.0.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-setupAnnoyingAPI("c74bafddda", "xyz.srnyx", "2.0.1", "Each player has a limited number of lives. If you die, you are punished")
+setupAnnoyingAPI("c74bafddda", "xyz.srnyx", "3.0.0", "Each player has a limited number of lives. If you die, you are punished")
 spigotAPI("1.8.8")
 repository(Repository.PLACEHOLDER_API)
 dependencies.compileOnly("me.clip", "placeholderapi", "2.11.3")
