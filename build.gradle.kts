0ee04cd11d044ba41e547a02fa40d99bedf2c5d5
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-12 17:39:07 -0400

    Update Annoying API
    5.0.0 -> 44b9fff69a

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("io.github.goooler.shadow") version "8.1.8"
 }
 
-setupAnnoyingAPI("5.0.0", "xyz.srnyx", "3.0.1", "Each player has a limited number of lives. If you die, you are punished")
+setupAnnoyingAPI("44b9fff69a", "xyz.srnyx", "3.0.1", "Each player has a limited number of lives. If you die, you are punished")
 spigotAPI("1.8.8")
 
 repository(Repository.PLACEHOLDER_API, Repository.ENGINE_HUB)
