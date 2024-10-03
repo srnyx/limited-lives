48b127af871ed5c222fe9e4952b552827d8a2ea3
srnyx <25808801+srnyx@users.noreply.github.com>
2024-10-03 13:38:06 -0400

    Update dependencies
    Gradle Galaxy: 1.2.2 -> 1.3.2
    Shadow: 8.1.8 -> 8.3.3
    PlaceholderAPI: 2.11.3 -> 2.11.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.2.2"
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 setupAnnoyingAPI("44b9fff69a", "xyz.srnyx", "3.0.1", "Each player has a limited number of lives. If you die, you are punished")
@@ -15,6 +15,6 @@ spigotAPI("1.8.8")
 
 repository(Repository.PLACEHOLDER_API, Repository.ENGINE_HUB)
 dependencies {
-    compileOnly("me.clip", "placeholderapi", "2.11.3")
+    compileOnly("me.clip", "placeholderapi", "2.11.6")
     compileOnly("com.sk89q.worldguard", "worldguard-bukkit", "7.0.0")
 }
