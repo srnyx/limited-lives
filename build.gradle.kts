3ab6ad80374cc4ee31b8da450a474976eb61f297
srnyx <25808801+srnyx@users.noreply.github.com>
2025-01-05 00:07:40 -0500

    Update Gradle and Gradle Galaxy
    Gradle: 8.11.1 -> 8.12
    Gradle Galaxy: 1.3.2 -> 1.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
     id("com.gradleup.shadow") version "8.3.5"
 }
 
