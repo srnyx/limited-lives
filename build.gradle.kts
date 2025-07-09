ab3258558eb56afeda4279958210c965d158c4c4
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-08 20:54:07 -0400

    Update dependencies
    Gradle: 8.12 -> 8.14.3
    Shadow: 8.3.5 -> 8.3.8
    Annoying API: 5.1.5 -> 2fa992b25a

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 plugins {
     java
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.5"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
-setupAnnoyingAPI("5.1.5", "xyz.srnyx", "4.0.0", "Each player has a limited number of lives. If you die, you are punished")
 spigotAPI("1.8.8")
+setupAnnoyingAPI("2fa992b25a", "xyz.srnyx", "4.0.0", "Each player has a limited number of lives. If you die, you are punished")
 
 repository(Repository.PLACEHOLDER_API, Repository.ENGINE_HUB)
 dependencies {
